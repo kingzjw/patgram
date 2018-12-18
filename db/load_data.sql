e3bde2116802dbbb270856df2e8fbab3bc3b0a48
king <zjwking258@163.com>
2018-12-18 16:37:13 +0800

    zjw version of patgram

---
+++ db/load_data.sql
@@ -0,0 +1,4 @@
+load data infile '/var/lib/mysql-files/verb.data' into table verb;
+load data infile '/var/lib/mysql-files/meaning_group.data' into table meaning_group;
+load data infile '/var/lib/mysql-files/example_verb.data' into table example_verb;
+load data infile '/var/lib/mysql-files/example_sentence.data' into table example_sentence;
